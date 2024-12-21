1be85b77395297113fea650ae68724c4277c5966
Ignacio Mercado <igmercado46@gmail.com>
2024-12-21 19:43:32 -0300

    fix: adds a proper real DB connection and needed environment variables

--- database/database.go
+++ database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"vaccine-api/models"
 
 	"gorm.io/driver/postgres"
@@ -9,11 +11,24 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_PORT := os.Getenv("DB_PORT")
 
+	fmt.Println("DB_PORT:", DB_PORT)
+
+	if DB_PORT == "" {
+		log.Fatal("DB_PORT is not set in environment variables")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Argentina/Buenos_Aires",
+		DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("failed to initialize database, got error: ", err.Error())
 	}
 
 	return db
--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"vaccine-api/database"
+	"vaccine-api/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
 	db := database.ConnectDatabase()
 
 	database.MigrateModels(db)
+
+	userHandler := handlers.UserHandler{DB: db}
 
 	router.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"status": "OK",
 		})
 	})
+
+	router.POST("/createUser", handlers.CreateUser(&userHandler))
 
 	router.Run()
 }
